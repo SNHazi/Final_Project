2c1ab1a949bfd5fbea4ff64b4ddaaf541c33bdfb
Hazirah <sitinurhazirah894@gmail.com>
2021-08-23 18:18:32 +0800

    forms added

---
+++ javascript/main.js
@@ -0,0 +1,45 @@
+
+
+
+
+
+
+
+
+//CONTACT
+document.querySelector("#submit").addEventListener("click", function (e) {
+    //dont do default behaviour
+    e.preventDefault();
+    let firstname = document.querySelector("#firstname");
+    //<input name="firstname" id="firstname" placeholder="John" />
+    validate(firstname);
+
+    let youremail = document.querySelector("#youremail");
+    //<input name="firstname" id="firstname" placeholder="John" />
+    validate(youremail);
+
+    let gender = document.querySelector("input[type=radio]");
+    //<input name="firstname" id="firstname" placeholder="John" />
+    validate(gender);
+
+    let message = document.querySelector("#message");
+    //<input name="firstname" id="firstname" placeholder="John" />
+    validate(message);
+    /**let firstname = document.querySelector("#firstname").value;
+    if (firstname.length < 2) {
+      document.querySelector("#label-firstname").classList.add("error");
+    }
+    let lastname = document.querySelector("#lastname").value;
+    if (lastname.length < 2) {
+      document.querySelector("#label-lastname").classList.add("error");
+    }*/
+  });
+
+  function validate(field) {
+    //field
+    //<input name="firstname" id="firstname" placeholder="John" />
+    let fieldName = field.name; //value of name in the above line
+    if (field.value.length < 2) {
+      document.querySelector("#label-" + fieldName).classList.add("error");
+    }
+  }
